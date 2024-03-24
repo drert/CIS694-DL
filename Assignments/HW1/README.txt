e99ebd93f52affb77fdad9936b3077cbd4738a14
drert <54976369+drert@users.noreply.github.com>
2024-03-24 15:45:46 -0400

    asdf
    asdf

---
+++ Assignments/HW1/README.txt
@@ -0,0 +1,10 @@
+Assignment 1 CI694 - Deep Learning
+Alexander Sukennyk
+
+All parts are completed to the best of my ability. 
+
+Output matches the requested format, graphing available for KMeans result (by changing draw_results to True).
+
+Any adjustable parameters which I have added are near the top of each script.
+
+No issues known. Computational errors have not been extensively tested for and may be present.
--- Assignments/HW1/myKMeans.py
+++ Assignments/HW1/myKMeans.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # additional parameters
+draw_results = False
 dist_exp = 2
-
 
 # make fake data by normal distribution (mean, std)
 n_data = torch.ones(1000, 2)
@@ -31,22 +31,26 @@ kmeans = KMeans(n_clusters=cluster_num, random_state=0, n_init='auto').fit(x.dat
 ################# 1 Implement your own kmeans clustering function myKmeans() (40 points) #################
 
 # Write your code here for your own kmeans clustering function
-def point_dist(x1: list,x2: list) :
-    # take in 2 numpy vectors, return distance
+def point_dist(x1, x2, numpy_arr= False) :
+    # take in 2 tensors, return distance
     if len(x1) != len(x2) :
         print("length mismatch")
         return None
     
     diff = x1 - x2
     exp = diff ** dist_exp
-    sum = torch.sum(exp)
+
+    if numpy_arr : # conditional for when calculating means later (using numpy arrays)
+        sum = np.sum(exp)
+    else :
+        sum = torch.sum(exp)
+
     dist = sum ** (1/dist_exp)
     return dist
 
 def plot_clusters(data, labels,K) :
     for mean_idx in range(K) : 
         filtered = np.array([point.data.numpy() for point,label in zip(data, labels) if label == mean_idx])
-        print(filtered)
         plt.scatter(filtered[:,0], filtered[:,1])
     plt.show()
     return
@@ -57,14 +61,48 @@ def plot_full (data) :
     plt.show()
     return
 
+def inter_intra_calc(data, labels, K, means) :
+    # intracluster calculation
+    inter,intra = 0,0
+
+    tot_intra = 0
+    counted = 0
+    for mean_idx in range(K) : 
+        filtered = np.array([point.data.numpy() for point,label in zip(data, labels) if label == mean_idx])
+        fil_len = len(filtered)
+        for i in range(fil_len) :
+            for j in range(fil_len-i-1) :
+                dist = point_dist(filtered[i],filtered[j+i+1],numpy_arr=True)
+                tot_intra += dist
+                counted += 1
+    intra = tot_intra / counted
+    # print("COUNTED: ", counted)
+
+    # intercluster calculation
+    tot_inter = 0
+    counted = 0
+    considered_vals = []
+    for i in range(K) :
+        for j in range(K-i-1) :
+            # print(i,j+i+1)
+            dist = point_dist(means[i],means[j+i+1], numpy_arr=True)
+            tot_inter += dist
+            counted += 1
+            # if dist in considered_vals :
+            #     print("DOUBLE COUNT")
+            # else :
+            #     considered_vals.append(dist)
+    inter = tot_inter / counted
+    # print("COUNTED: ", counted)
+    return inter, intra
+
 def myKmeans(x, K, max_iteration=1000):
     # write your code here based on the algorithm described in class, and you cannot call other kmeans clustering packages here
 
     # initialize random centers 
-    #   - i just select random points from the dataset to ensure that we have some reasonable start position
+    #   - i sample points (without replacement) from the dataset to ensure that we have some reasonable start position
     idxs = torch.randperm(len(x))[:K]
     means = x[idxs].data.numpy()
-    print(means)
 
     # initialize trackers
     iter = 0
@@ -74,10 +112,10 @@ def myKmeans(x, K, max_iteration=1000):
     labels = np.zeros(x.size(dim=0))
 
     # initial plot
-    plot_full(x)
+    # plot_full(x)
 
     # start of main iteration loop
-    while (iter < 10) : # max_iteration) and (converged == False) :
+    while (iter < max_iteration) and (converged == False) :
         # assignment step - assign each observation to the nearest mean
         for x_idx in range(x.size(dim=0)) :
             dists = [point_dist(x[x_idx], mean_i) for mean_i in means]
@@ -90,23 +128,29 @@ def myKmeans(x, K, max_iteration=1000):
         
         # update step - calculate new means of the re-assigned observations in the new clusters
         new_means = np.array([sum_/count_ for sum_,count_ in zip(sums, counts)])
-        print(new_means)
-
+        
         # check for convergence
         if np.array_equal(means, new_means, equal_nan=False) :
             converged = True
-
-        # checker plot
-        plot_clusters(x, labels, K)
         
+        # tester printouts, plotting
+        # if iter % 5 == 0:
+        #     print(iter)
+        # if iter % 10 == 0 :
+        #     plot_clusters(x, labels, K)
+            
+
         # update iteration, reset trackers, update means
         iter += 1
         means = new_means
         sums = np.zeros((K, x.size(dim=1))) # 
         counts = np.zeros(K)
 
-
-    center, mean_intra_cluster_distance, mean_inter_cluster_distance = 0,0,0
+    if (draw_results) :
+        plot_clusters(x, labels, K)
+    
+    center = means
+    mean_inter_cluster_distance, mean_intra_cluster_distance = inter_intra_calc(x, labels, K, means)
     return center, mean_intra_cluster_distance, mean_inter_cluster_distance
 
 
@@ -117,7 +161,15 @@ def myKmeans(x, K, max_iteration=1000):
 # print the ratio of mean_intra_cluster_distance over mean_inter_cluster_distance for each K.
 # print the optimal K with minimum ratio
 
-center,intra,inter = myKmeans(x,5)
-# for i in range(10) :
-#     center,intra,inter =myKmeans(x,i)
-#     print(intra/inter)
+best_ratio = 1000000
+best_K = 0
+for i in range(2,10) :
+    center,intra,inter =myKmeans(x,i)
+    if intra/inter < best_ratio :
+        best_K = i
+        best_ratio = intra/inter
+    print("When K =", i, "mean intra-cluster distance:", intra, 
+                         ",mean inter-cluster distance:", inter, 
+                         ", ratio:", intra/inter)
+
+print("Best intra/inter-cluster ratio achieved at K =", best_K, "ratio =", best_ratio)
